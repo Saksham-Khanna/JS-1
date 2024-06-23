c86479fe56ef1f46cfc09713abe91e98eb88d50f
Saksham-Khanna <68804085+Saksham-Khanna@users.noreply.github.com>
2024-06-23 06:45:59 +0000

    datatype conversion

--- .devcontainer/basic-js/03_conversionOperation.js
+++ .devcontainer/basic-js/03_conversionOperation.js
@@ -1,4 +1,31 @@
-let score = 23
+// let score = 23
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valinNumer = String(score)
+// console.log(typeof valinNumer)
+
+// let age = "123nda"
+
+// let age2 = Number(age)
+// console.log(typeof age)
+// console.log(age2) // not a number NaN
+
+let age3 = undefined
+console.log(typeof age3)
+
+let age4 = null
+console.log(typeof age4)
+
+let age5 = true
+console.log(typeof age5);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1 ; false => 0
+
+let logged = ""
+
+let bool = Boolean(logged)
+console.log(bool)
