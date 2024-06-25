eba646c740f84beffaede771414ee8fe06b37311
Saksham-Khanna <68804085+Saksham-Khanna@users.noreply.github.com>
2024-06-25 07:49:40 +0000

    operations

--- .devcontainer/basic-js/03_conversionOperation.js
+++ .devcontainer/basic-js/03_conversionOperation.js
@@ -12,20 +12,42 @@
 // console.log(typeof age)
 // console.log(age2) // not a number NaN
 
-let age3 = undefined
-console.log(typeof age3)
+// let age3 = undefined
+// console.log(typeof age3)
 
-let age4 = null
-console.log(typeof age4)
+// let age4 = null
+// console.log(typeof age4)
 
-let age5 = true
-console.log(typeof age5);
+// let age5 = true
+// console.log(typeof age5);
 
 // "33" => 33
 // "33ab" => NaN
 // true => 1 ; false => 0
 
-let logged = ""
+// let logged = ""
 
-let bool = Boolean(logged)
-console.log(bool)
+// let bool = Boolean(logged)
+// console.log(bool)
+
+// let someNo = 33
+// let stringNo = String(someNo)
+
+// console.log(stringNo)
+// console.log(typeof stringNo);
+
+// ************************************OPERATIONS*********************************************//
+// let value = 5
+// let negval = -value
+// // console.log(negval);
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "Hello"
+let str2 = "Hey"
+
+let str3 = str1 + str2
