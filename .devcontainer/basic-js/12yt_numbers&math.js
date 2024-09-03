df0e0d4c6d0732e809eed20ed1ceeed81e7119d2
Saksham-Khanna <68804085+Saksham-Khanna@users.noreply.github.com>
2024-09-03 11:33:15 +0530

    Create 12yt_numbers&math.js

---
+++ .devcontainer/basic-js/12yt_numbers&math.js
@@ -0,0 +1,11 @@
+console.log(Math);
+console.log(Math.abs(-100));
+console.log(Math.round(5.9));
+console.log(Math.round(5.2));
+console.log(Math.round(5.5));
+// round to next - ceil , round to lower - floor
+console.log(Math.floor(4.9));
+console.log(Math.ceil(4.1));
+
+console.log(Math.min(1,45,6,8,2,67,1,0,-14));
+console.log(Math.max(1,45,6,8,2,67,1,0,-14));
